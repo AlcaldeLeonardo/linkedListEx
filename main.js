37dce96752a23d4da3d81767d5de756e010cefc3
Alcalde Gomez Leonardo Esteban <leonardoalcalde020@gmail.com>
2024-10-31 18:11:47 -0300

    final test

--- main.js
+++ main.js
@@ -2,22 +2,16 @@ import { LinkedList } from './LinkedList.js'
 
 const linkedList = new LinkedList()
 
-console.log(linkedList)
-console.log(linkedList.head())
-console.log(linkedList.tail())
-
 linkedList.prepend(5)
 linkedList.prepend(30)
 linkedList.append("I'm the tail")
 linkedList.prepend(40)
-console.log(linkedList.tail())
-
-linkedList.prepend(54)
 linkedList.append('im the tail now')
-
-console.log(linkedList)
-
-console.log(linkedList.head())
-console.log(linkedList.tail())
-console.log('Size: ', linkedList.size())
-console.log(linkedList.at(5))
+linkedList.pop()
+console.log(linkedList.find(40))
+console.log(linkedList.toString())
+linkedList.insertAt('90', 4)
+console.log(linkedList.toString())
+console.log(linkedList.size())
+linkedList.removeAt(0)
+console.log(linkedList.toString())
